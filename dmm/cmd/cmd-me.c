4cfc1763e1f06c6ac485b6b64856fd0ebc4ad781
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-10-03 17:11:24 +0900

    dmm: cmd-me: use Linux code style
    For tab/space.
    By using ${LINUX}/scripts/checkpatch.pl
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- dmm/cmd/cmd-me.c
+++ dmm/cmd/cmd-me.c
@@ -1,7 +1,7 @@
 /************************************************************************
 
 
-                                 me
+			me
 
 2008/09  morimoto
 ************************************************************************/
@@ -9,132 +9,132 @@
 
 //=====================================
 //
-//          help
+//	help
 //
 //=====================================
 static STHELPMSG s_Help[] = {
-    {
-        "me" , "memory edit" ,
-        "dmm me[.d, .w, .l] addr data\n"
-        "    addr  : address\n"
-        "    data  : data\n"
-        "ex) dmm me.w 0x89000000 0x1234\n"
-        "    dmm me   0x89000000 0x12345678  # it mean LONG\n"
-        "\n"
-        "you can use mask and set by -m option\n"
-        "dmm me -m 0x89000000 0xFFFF0000 0x12340000\n"
-    },
-    HELPMSGEND
+	{
+		"me", "memory edit",
+		"dmm me[.d, .w, .l] addr data\n"
+		"    addr  : address\n"
+		"    data  : data\n"
+		"ex) dmm me.w 0x89000000 0x1234\n"
+		"    dmm me   0x89000000 0x12345678  # it mean LONG\n"
+		"\n"
+		"you can use mask and set by -m option\n"
+		"dmm me -m 0x89000000 0xFFFF0000 0x12340000\n"
+	},
+	HELPMSGEND
 };
 
 //=====================================
 //
-//               cmd
+//	cmd
 //
 //=====================================
-static bool cmd( int nArgc, char *pstrArgv[] )
+static bool cmd(int nArgc, char *pstrArgv[])
 {
 
-    uintptr_t     addr;
-    enum ESTYPE   stype;
-    u32           data;
-    u32           data_check  = 0xFFFFFFFF;
-    u32           data_mask   = 0;
-    STMEMCTRL    *pmctrl = NULL;
-    bool          ret    = false;
-    int           i      = 0;
+	uintptr_t	addr;
+	enum ESTYPE	stype;
+	u32		data;
+	u32		data_check = 0xFFFFFFFF;
+	u32		data_mask = 0;
+	STMEMCTRL	*pmctrl = NULL;
+	bool		ret = false;
+	int		i = 0;
 
-    stype  = CmdGetDataSize( pstrArgv[i++] , TLONG );
+	stype = CmdGetDataSize(pstrArgv[i++], TLONG);
 
-    if ( 0 == strcmp( "-m", pstrArgv[i]))
-        data_mask = (u32)i++;
+	if (strcmp("-m", pstrArgv[i]) == 0)
+		data_mask = (u32)i++;
 
-    addr   = GetAddress( pstrArgv[i++] );
+	addr = GetAddress(pstrArgv[i++]);
 
-    if ( data_mask )
-        data_mask = GetData( pstrArgv[i++] );
+	if (data_mask)
+		data_mask = GetData(pstrArgv[i++]);
 
-    data   = GetData( pstrArgv[i++] );
+	data = GetData(pstrArgv[i++]);
 
-    if ( nArgc != i ) {
-        printf( "program error\nneeded re-check %d %d\n", nArgc, i );
-        return false;
-    }
+	if (nArgc != i) {
+		printf("program error\nneeded re-check %d %d\n", nArgc, i);
+		return false;
+	}
 
-    if ( TERR == stype )
-        return Usage( "type error" );
+	if (stype == TERR)
+		return Usage("type error");
 
-    if( !MemInit(  ))
-        return Error( "mem ctrl init error" );
+	if (!MemInit())
+		return Error("mem ctrl init error");
 
-    pmctrl = GetMemCtrl( stype );
-    if ( !pmctrl ) {
-        Error( "get mem ctrl error" );
-        goto error;
-    }
+	pmctrl = GetMemCtrl(stype);
+	if (!pmctrl) {
+		Error("get mem ctrl error");
+		goto error;
+	}
 
-    if ( !addr ) {
-        Error( "addr error" );
-        goto error;
-    }
-    if ( addr & (pmctrl->nIncSize-1) ) {
-        Error( "address alignment error" );
-        goto error;
-    }
+	if (!addr) {
+		Error("addr error");
+		goto error;
+	}
+	if (addr & (pmctrl->nIncSize-1)) {
+		Error("address alignment error");
+		goto error;
+	}
 
-    /*
-     * CAUTION
-     *
-     * we can not use 32 bit shift.
-     * this operation is undefined in C !!
-     * So, this time we use 8bit shift and loop
-     */
-    for ( i=0 ; i<pmctrl->nIncSize; i++ )
-        data_check = data_check << 8;
+	/*
+	 * CAUTION
+	 *
+	 * we can not use 32 bit shift.
+	 * this operation is undefined in C !!
+	 * So, this time we use 8bit shift and loop
+	 */
+	for (i = 0; i < pmctrl->nIncSize; i++)
+		data_check = data_check << 8;
 
-    if ( data & data_check ) {
-        Error( "input data error" );
-        goto error;
-    }
+	if (data & data_check) {
+		Error("input data error");
+		goto error;
+	}
 
-    if ( data_mask & data_check ) {
-        Error( "data mask error" );
-        goto error;
-    }
+	if (data_mask & data_check) {
+		Error("data mask error");
+		goto error;
+	}
 
-    if ( data_mask ) {
-        u32 current;
+	if (data_mask) {
+		u32 current;
 
-        if ( !pmctrl->fnRead( addr , &current ) )
-            goto error;
+		if (!pmctrl->fnRead(addr, &current))
+			goto error;
 
-        data = (current & ~data_mask) | (data & data_mask);
-    }
+		data = (current & ~data_mask) | (data & data_mask);
+	}
 
-    pmctrl->fnEdit( addr , &data );
-    ret = true;
+	pmctrl->fnEdit(addr, &data);
+	ret = true;
 
- error:
-    MemExit(  );
-    return ret;
+error:
+	MemExit();
+	return ret;
 }
 
 //=====================================
 //
-//          ishit
+//	ishit
 //
 //=====================================
 static bool
-ishit( int nArgc, char *pstrArgv[] )
+ishit(int nArgc, char *pstrArgv[])
 {
-    if ((CMD_HIT( pstrArgv[0] , "me.b" ) ||
-         CMD_HIT( pstrArgv[0] , "me.w" ) ||
-         CMD_HIT( pstrArgv[0] , "me.l" ) ||
-         IsCmdHit( ))
-        &&
-        (( 3 == nArgc ) || ( 5 == nArgc )))
-        return true;
+	if ((CMD_HIT(pstrArgv[0], "me.b") ||
+	     CMD_HIT(pstrArgv[0], "me.w") ||
+	     CMD_HIT(pstrArgv[0], "me.l") ||
+	     IsCmdHit())
+	    &&
+	    ((nArgc == 3) || (nArgc == 5)))
+		return true;
 
-    return false;
+	return false;
 }
-INSTALL_CMD( me )
+INSTALL_CMD(me)
